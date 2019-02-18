e6f9dde3d2a189cf1218d70c34c3136ac7444dfe
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-02-18 16:14:30 +0200

    Start js video player

---
+++ app/js/video.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var videoContainer = document.getElementById("video-container");
+var videoPlayer = videoContainer.querySelector(".video__player");
+var video = videoContainer.querySelector(".video__player video");
+var customControls = videoContainer.querySelector(".video__custom-controls");
+var playBtn = videoContainer.querySelector(".video__play");
+var pauseBtn = videoContainer.querySelector(".video__pause");
+var fullScreenBtn = videoContainer.querySelector(".video__full-screen");
+var timeLine = videoContainer.querySelector(".video__timline-inner");
+var timeString = videoContainer.querySelector(".video__time");
+
+function addCustomControls() {}
---
+++ app/scripts/video.js
@@ -0,0 +1,11 @@
+let videoContainer = document.getElementById("video-container");
+let videoPlayer = videoContainer.querySelector(".video__player");
+let video = videoContainer.querySelector(".video__player video");
+let customControls = videoContainer.querySelector(".video__custom-controls");
+let playBtn = videoContainer.querySelector(".video__play");
+let pauseBtn = videoContainer.querySelector(".video__pause");
+let fullScreenBtn = videoContainer.querySelector(".video__full-screen");
+let timeLine = videoContainer.querySelector(".video__timline-inner");
+let timeString = videoContainer.querySelector(".video__time");
+
+function addCustomControls() {}
